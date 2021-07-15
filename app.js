afddb75871b081e24761390cd8681ca62b61408f
haslozik <wojciechperlinskitpel@gmail.com>
2021-07-15 22:45:44 +0200

    menu

---
+++ app.js
@@ -0,0 +1,16 @@
+/* menu */
+const menu = document.getElementById("menu"); 
+const menuOpener = document.getElementById("firstView__topMenuButton");
+const menuClose = document.getElementById("menu__close");
+
+    function openMenu() {
+        menu.style.height = '100vh';
+        menuOpener.style.display = 'none';
+        menuClose.style.display = 'block';
+    }
+
+    function closeMenu() {
+        menu.style.height = '0vh';
+        menuOpener.style.display = 'block';
+        menuClose.style.display = 'none';
+    }
