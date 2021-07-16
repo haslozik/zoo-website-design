1ee2290d6bd7ded90e03e7f44e07db3c74da71e6
haslozik <wojciechperlinskitpel@gmail.com>
2021-07-16 19:29:38 +0200

    menu animation added

--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 const menu = document.getElementById("menu"); 
 const menuOpener = document.getElementById("firstView__topMenuButton");
 const menuClose = document.getElementById("menu__close");
+const menuList = document.getElementById("menu__itemsList");
 
     function openMenu() {
         menu.style.height = '100vh';
         menuOpener.style.display = 'none';
         menuClose.style.display = 'block';
+
+        setTimeout(() => {
+            menuList.style.opacity = '1';
+        }, 500);
     }
 
     function closeMenu() {
         menu.style.height = '0vh';
         menuOpener.style.display = 'block';
         menuClose.style.display = 'none';
+        menuList.style.opacity = '0';
     }
 
     
